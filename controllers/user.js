6083bbaea90cc677ab3f3b20c779fe6fbc71f9ba
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-02-28 01:52:22 +0300

    düzenleme

--- controllers/user.js
+++ controllers/user.js
@@ -86,24 +86,3 @@ exports.login = async (req, res) => {
       .json({ message: "An error occurred while logging in the user" });
   }
 };
-
-exports.isAuth = (req, res, next) => {
-  const authHeader = req.get("Authorization");
-  if (!authHeader) {
-    return res.status(401).json({ message: "not authenticated" });
-  }
-  const token = authHeader.split(" ")[1];
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(token, "secret");
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: err.message || "could not decode the token" });
-  }
-  if (!decodedToken) {
-    res.status(401).json({ message: "unauthorized" });
-  } else {
-    res.status(200).json({ message: "here is your resource" });
-  }
-};
